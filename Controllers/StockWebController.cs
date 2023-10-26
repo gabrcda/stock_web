e89392d46909a4b0fbb1567432011fcb45191674
unknown <gabri22kocebrac@gmail.com>
2023-10-26 10:39:54 -0300

    add initial project files

---
+++ Controllers/StockWebController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+// using stock_web.Models;
+
+namespace stock_web.Controllers;
+
+public class StockWebController : Controller
+{
+    public IActionResult Principal()
+    {
+        return View();
+    }
+
+    public IActionResult Categorias()
+    {
+        return View();
+    }
+
+    public IActionResult Produtos()
+    {
+        return View();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=StockWeb}/{action=Principal}/{id?}");
+
+app.Run();
